4a16ea8e9e130d3b1897c77af0c2a6fe34949c0d
gunark <gunark@0fc6f807-a9c6-43cf-8b64-6dd3cb0ba993>
2007-07-31 19:06:53 +0000

    added plugin installation instructions
    git-svn-id: http://axis-netcam.rubyforge.org/svn/trunk@14 0fc6f807-a9c6-43cf-8b64-6dd3cb0ba993

--- lib/axis-netcam/init.rb
+++ lib/axis-netcam/init.rb
@@ -1,3 +1,9 @@
 # This file makes it possible to install axis-netcam as a Rails plugin.
+#
+# To install as a plugin:
+# 
+#   cd <your Rails application's root directory>
+#   ruby script/plugin install -x http://axis-netcam.rubyforge.org/svn/trunk/lib/axis-netcam
+#
 
 require File.dirname(File.expand_path(__FILE__)) + '/camera'
